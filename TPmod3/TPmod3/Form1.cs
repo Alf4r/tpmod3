d75c2f7e57f30065ae128571cc7b318355d7ffd9
Alf4r <malfarizky23@gmail.com>
2024-03-04 02:31:35 +0700

    commit akhir nambah fitur gui

---
+++ TPmod3/TPmod3/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TPmod3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string inputText = textBox1.Text; // Fetch text from textBox1
+            if (inputText.ToUpper() == "Alfar")
+            {
+                label1.Text = " Alfar"; // Assign text to label1's Text property
+            }
+            else
+            {
+                label1.Text = "HALO " + inputText; // Assign text to label1's Text property
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
